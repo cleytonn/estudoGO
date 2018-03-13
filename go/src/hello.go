e4397f74f60467b62673b863c290de3a0c5692cd
Cleytonn <cleyton.henry@gmail.com>
2018-03-13 02:12:39 -0300

    Criando o Menu

--- go/src/hello.go
+++ go/src/hello.go
@@ -1,11 +1,22 @@
-package main
+package main //Pacote Principal
 
-import "fmt"
+import "fmt" //Pacote de formatação, como: print e scanf
 
 func main() {
-	var nome string = "Cleyton"
-	var versao float32 = 1.1
+	nome := "Cleyton"
+	versao := 1.1
 
 	fmt.Println("Olá,", nome)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("\n1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do Programa\n")
+
+	var comando int
+
+	fmt.Scan(&comando)
+
+	fmt.Println("\nO valor da variável comando é:", comando)
+
 }
